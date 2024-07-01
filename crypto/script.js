cf74c0e0001df76169b9d619be41f4e8c7f9dd65
liviagalletti <liligalletti@gmail.com>
2024-07-01 11:08:23 -0300

    api

--- crypto/script.js
+++ crypto/script.js
@@ -1,1 +1,17 @@
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
 
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DOGE&tsyms=USD,EUR",
+    "method": "GET",
+    "headers": {}
+}
+
+$.ajax(settings).done(function(response) {
+    btc.innerHTML = response.BTC.USD;
+    eth.innerHTML = response.ETH.USD;
+    doge.innerHTML = response.DOGE.USD;
+});
